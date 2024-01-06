1e45620c9530f4257fab629fec0a1ea44985330f
fk0725 <anita.liberatore@consulenti.allitude.it>
2024-01-06 14:01:18 +0100

    add form reactive form

--- src/app/components/dialog/dialog-add-edit.component.ts
+++ src/app/components/dialog/dialog-add-edit.component.ts
@@ -1,20 +1,22 @@
-import {Component} from '@angular/core';
-import {MatDialogModule} from '@angular/material/dialog';
+import {Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from "@angular/material/input";
 import {MatDatepickerModule} from "@angular/material/datepicker";
 import {MatRadioModule} from "@angular/material/radio";
 import {MatSelectModule} from "@angular/material/select";
 import {NgForOf} from "@angular/common";
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-dialog-add-edit',
   templateUrl: './dialog-add-edit.component.html',
   styleUrls: ['./dialog-add-edit.component.scss'],
   standalone: true,
-  imports: [MatButtonModule, MatDialogModule, MatInputModule, MatDatepickerModule, MatRadioModule, MatSelectModule, NgForOf],
+  imports: [MatButtonModule, MatDialogModule, MatInputModule, MatDatepickerModule, MatRadioModule, MatSelectModule, NgForOf, ReactiveFormsModule],
 })
 export class DialogAddEditComponent {
+  empForm: FormGroup;
 
   education: string[] = [
     'Matric',
@@ -24,4 +26,26 @@ export class DialogAddEditComponent {
     'Post Graduate',
   ];
 
+  constructor(
+    private _fb: FormBuilder
+  ) {
+    this.empForm = this._fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      dob: '',
+      gender: '',
+      education: '',
+      company: '',
+      experience: '',
+      package: '',
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onFormSubmit() {
+    console.log(this.empForm)
+  }
 }
